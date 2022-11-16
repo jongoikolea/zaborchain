5882e1f01e961744ac63b637072fea40fde10970
atxurielektronika <118360013+atxurielektronika@users.noreply.github.com>
2022-11-16 13:28:41 +0100

    Update ledControl.sol

--- code/smartContracts/ledControl.sol
+++ code/smartContracts/ledControl.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+ 
+contract LedControl {
+ 
+   bool ledStatus;
+ 
+   function update (bool newS) public {
+       ledStatus = newS;
+   }
+ 
+   function read () public view returns (bool){
+       return ledStatus;
+   }
+ 
+}
 
