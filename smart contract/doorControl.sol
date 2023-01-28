0090a413fd5585125c85f38a293154e61e185609
Jon Goikolea <118360013+jongoikolea@users.noreply.github.com>
2023-01-28 12:33:53 +0100

    Create doorControl.sol

---
+++ smart contract/doorControl.sol
@@ -0,0 +1,22 @@
+/*doorControl.sol es un smart contract Ethereum simple.
+*la variable doorStatus guarda el estado de la puerta del contenedor abierto(true)/cerrado(false)
+*la función update() sirve para modificar el estado de doorStatus
+*/con la funcion read() leemos la variable doorStatus
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract DoorControl {
+
+   bool doorStatus;
+
+   function update (bool newS) public {
+       doorStatus = newS;
+   }
+
+   function read () public view returns (bool){
+       return doorStatus;
+   }
+
+}
